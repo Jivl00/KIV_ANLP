9b32b688a0045254f1247b17f081e1575da40eb1
Jivl00 <vladimira.kimlova@seznam.cz>
2024-09-26 15:39:22 +0200

    histogram prettify

--- cv01/class_histogram.py
+++ cv01/class_histogram.py
@@ -9,23 +9,22 @@ transform = transforms.Compose([
 dataset1 = datasets.MNIST('data', train=True, download=True, transform=transform)
 dataset2 = datasets.MNIST('data', train=False, transform=transform)
 
-targets = torch.cat((dataset1.targets, dataset2.targets))
-# print(len(targets))
-# class_1 = 0
-# for i in range(len(targets)):
-#     if targets[i] == 1:
-#         class_1 += 1
-# print(class_1)
+targets1 = dataset1.targets
+targets2 = dataset2.targets
 
-class_hist = torch.bincount(targets)
-plt.figure(figsize=(10, 6))
-plt.bar(range(len(class_hist)), class_hist, color='skyblue', edgecolor='black')
-for i, v in enumerate(class_hist):
-    plt.text(i, v + 0.5, str(v.item()), ha='center', va='top', fontsize=12, rotation=90)
-    plt.xlabel("Class", fontsize=14)
+class_hist1 = torch.bincount(targets1, minlength=10)
+class_hist2 = torch.bincount(targets2, minlength=10)
+
+plt.figure(figsize=(10, 8))
+plt.bar(range(len(class_hist1)), class_hist1, color='skyblue', edgecolor='black', label='Train')
+plt.bar(range(len(class_hist2)), class_hist2, color='lime', edgecolor='black', bottom=class_hist1, label='Test')
+for i, (v1, v2) in enumerate(zip(class_hist1, class_hist2)):
+    plt.text(i, v1 + 0.5, str(v1.item()), ha='center', va='top', fontsize=12, rotation=90)
+    plt.text(i, v1 + v2 + 0.5, str(v2.item()), ha='center', va='top', fontsize=12, rotation=90)
 plt.ylabel("Count", fontsize=14)
 plt.title("Class Histogram", fontsize=16)
-plt.xticks(range(len(class_hist)), fontsize=12)
+plt.xticks(range(len(class_hist1)), fontsize=12)
 plt.yticks(fontsize=12)
 plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.legend(fontsize=12, loc='lower right')
 plt.show()
